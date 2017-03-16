e3158b4564344b1f6be33a8af19f09f2a1114499
Shadi Khaled <shadikhaled777@gmail.com>
2017-03-16 12:10:12 +0300

    Done.

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { key: 'value' }
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+
+  return playlist;
+}
+function removeFromPlaylist(playlist,artistName){
+	delete playlist[artistName];
+	return playlist;
+
+}
